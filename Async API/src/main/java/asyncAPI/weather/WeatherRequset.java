43dba6180dd8bb114462a1c6b96b1b0221786fa1
Semyon Inokov <semen.inokov@gmail.com>
2025-10-12 23:52:41 +0700

    Add creating weather request

---
+++ Async API/src/main/java/asyncAPI/weather/WeatherRequset.java
@@ -0,0 +1,14 @@
+package asyncAPI.weather;
+
+import okhttp3.Request;
+
+public class WeatherRequset {
+    private static final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private static final String apikey = "-";
+
+    public static Request getWeather(double lat, double lon) {
+        String uri = url + "?lat=" + lat + "&lon=" + lon + "&appid=" + apikey + "&units=metric&lang=ru";
+        Request request = new Request.Builder().url(uri).get().build();
+        return request;
+    }
+}
