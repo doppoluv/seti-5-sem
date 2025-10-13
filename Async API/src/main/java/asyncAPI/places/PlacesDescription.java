22a64961189ec0aebbf8eaba4824e984f05d31ca
Semyon Inokov <semen.inokov@gmail.com>
2025-10-13 15:06:00 +0700

    Fixed tabs

--- Async API/src/main/java/asyncAPI/places/PlacesDescription.java
+++ Async API/src/main/java/asyncAPI/places/PlacesDescription.java
@@ -3,19 +3,19 @@ package asyncAPI.places;
 import com.google.gson.annotations.SerializedName;
 
 public class PlacesDescription {
-        @SerializedName("info")
-        private Info info;
+    @SerializedName("info")
+    private Info info;
 
-        public Info getInfo() {
-            return info;
-        }
+    public Info getInfo() {
+        return info;
+    }
 
-        public static class Info {
-            @SerializedName("descr")
-            private String description;
+    public static class Info {
+        @SerializedName("descr")
+        private String description;
 
-            public String getDescription() {
-                return description != null ? description : "";
-            }
+        public String getDescription() {
+            return description != null ? description : "";
         }
     }
+}
